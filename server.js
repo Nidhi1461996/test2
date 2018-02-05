9ba799703e1d6197f8a98268d78d6f1745f88375
Nidhi Jagadeesha <nidhijune14@gmail.com>
2018-02-05 13:15:43 +0530

    received the ids from api1

--- server.js
+++ server.js
@@ -8,16 +8,35 @@ server.connection({ port: 8085 });
 function handlerFunc(request, reply) {
   // axios.get('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks')
   //   .then((response) => {
-  //     console.log(response.data);
-  //     reply(response.data);
+  //     console.log(response);
+  //     reply(response);
   //   });
-  http.get('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks', (response) => {
-    response.on('data', (data) => {
-      response.setEncoding('UTF8');
-      console.log(data.payload);
-      reply(data.payload);
+  let data = '';
+  http.get('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks', (resp) => {
+    resp.on('data', (data1) => {
+      data += data1;
+      resp.on('end', () => {
+        console.log(JSON.parse(data));
+        data = JSON.parse(data);
+        len = data.books.length;
+        const ids = [];
+        // for (let i = 0; i < len; i++) {
+        //   //ids[i] = data.books[i].id;
+        //   http.get('')
+        // }
+        reply(ids);
+      });
+    }).on('error', (error) => {
+      console.log(`Error is: ${error.message}`);
     });
   });
+  // http.get('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks', (response) => {
+  //   response.on('data', (data) => {
+  //     response.setEncoding('UTF8');
+  //     console.log(JSON.parse(data));
+  //     reply(JSON.parse(data));
+  //   });
+  // });
 }
 
 server.route({ method: 'GET', path: '/', handler: handlerFunc });
